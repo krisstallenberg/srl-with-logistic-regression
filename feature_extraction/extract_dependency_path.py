ab6cb5d4c03111b188cd5aabd3a742661afd551d
Farnaz-BNF <160543501+Farnaz-BNF@users.noreply.github.com>
2024-02-22 22:38:23 +0330

    Update extract_dependency_path added padding to output of function

--- feature_extraction/extract_dependency_path.py
+++ feature_extraction/extract_dependency_path.py
@@ -7,15 +7,18 @@ def find_dependency_path(token_id, sentence,pred_id ):
     
     Return:
         List: a dependency path from token_id to pred_id.
+        Using padding, list has always len of 20. Because MAX dependency path in all used datasets is 17.
 
     sentence (dict): The sentence object
     token_id : The id of the word in the sentence
     pred_id : The id of the predicate in the sentence
     """
-    dep_path = []
+    dep_path = ['']*20
     current_id = token_id
+    idx=0
     # Starts from token_id and each time 'form' and 'XPOS' of that token is added to dependency path list.
     # Then, token becomes head of the current token. It continues until head is the predicate.
+    # list with len of 20 with empty strings is created, then from beginning of list empty strings are replaced with the path.
     while current_id != 0:
         if int(current_id)-1 == 0:
             break
@@ -24,7 +27,8 @@ def find_dependency_path(token_id, sentence,pred_id ):
         if token is None:
             break
 
-        dep_path.insert(len(dep_path), f"{token['xpos']}")
+        dep_path[idx] = (f"{token['xpos']}")
+        idx +=1
         current_id = int(token['head'])
 
     return dep_path
@@ -45,19 +49,19 @@ def extract_dependency_path(sentence):
         if word['predicate'] != '_':
             pred_id = i
     # For each word in the sentence, if it is not root or prediate, finds its dependency path to predicate.
-    # If it is root or predicate, its dependency path is '_'.
+    # If it is root or predicate, its dependency path is a list of empty strings with len of 20.
     for i, word in enumerate(sentence):
         if word['form'] != sentence[int(pred_id)]['form']:
             dep_path = find_dependency_path(word['id'], sentence,pred_id)
             if dep_path:
                 features.append(dep_path)
 
-            # If the current word is the root, its dependency path is '_'
+            # If the current word is the root, its dependency path is a list of empty strings with len of 20
             else:
-                features.append('_')
+                features.append(['']*20)
 
-        # If the current word is the predicate, its dependency path is '_'
+        # If the current word is the predicate, its dependency path is a list of empty strings with len of 20
         else:
-            features.append('_')
+            features.append(['']*20)
 
     return features
