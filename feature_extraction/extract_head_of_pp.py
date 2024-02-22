90710ebc9a92d95c244e6fdf70b3a1b274c667ef
Christina-Karavida <160549443+Christina-Karavida@users.noreply.github.com>
2024-02-22 19:48:05 +0100

    Update extract_head_of_pp.py

--- feature_extraction/extract_head_of_pp.py
+++ feature_extraction/extract_head_of_pp.py
@@ -1,24 +1,23 @@
-def head_word_of_pp(parsed_sentence):
+def head_word_of_pp(sentence):
     """
     Extracts the head word of the first noun phrase inside each PP and appends the preposition information to the phrase type
 
-    Returns a list containing of features representing the head word of each PP
-
+    Returns a list containing features representing the head word of each PP
     """
     features = []  
-    for token_info in parsed_sentence:
+    for token_info in sentence:
         if token_info['dependency_relation'] == 'case':  #it first checks if the token is a preposition
             pp_head_word = None
-            for prep_head_relation in parsed_sentence:#looking for the head word of the pp
+            for prep_head_relation in sentence:#looking for the head word of the pp
                 if (prep_head_relation['head'] == token_info['id'] and #if the token's head matches the id of the pp token 
-                        prep_head_relation['dependency_relation'] != 'punct'): #and is not a punctuation mark
-                    pp_head_word = prep_head_relation['form']  #it stores it as the head word of the pp
+                        prep_head_relation['dependency_relation'] != 'punct'):  #and is not a punctuation mark
+                    pp_head_word = prep_head_relation['form']#it stores it as the head word of the pp
                     break
-            if pp_head_word: #for every head word that is founs
-                feature = f"PP-{token_info['form']}-{pp_head_word}"  #it appends the preposition and the head to the feature
+            if pp_head_word:
+                feature = f"PP-{token_info['form']}-{pp_head_word}"#it appends the preposition and the head to the feature
             else:
-                feature = f"PP-{token_info['form']}"  #otherwise it only includes the preposition
+                feature = f"PP-{token_info['form']}" #otherwise it only includes the preposition
             features.append(feature)  
-    return features 
+    return features
 
 
