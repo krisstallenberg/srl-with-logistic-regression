7b1c192add7f115d0778e99ad829bbfa4f61b209
Christina-Karavida <160549443+Christina-Karavida@users.noreply.github.com>
2024-02-22 19:27:03 +0100

    Update extract_head_of_pp.py

--- feature_extraction/extract_head_of_pp.py
+++ feature_extraction/extract_head_of_pp.py
@@ -12,7 +12,7 @@ def head_word_of_pp(parsed_sentence):
             for prep_head_relation in parsed_sentence:#looking for the head word of the pp
                 if (prep_head_relation['head'] == token_info['id'] and #if the token's head matches the id of the pp token 
                         prep_head_relation['dependency_relation'] != 'punct'): #and is not a punctuation mark
-                    pp_head_word = dependent_info['form']  #it stores it as the head word of the pp
+                    pp_head_word = prep_head_relation['form']  #it stores it as the head word of the pp
                     break
             if pp_head_word: #for every head word that is founs
                 feature = f"PP-{token_info['form']}-{pp_head_word}"  #it appends the preposition and the head to the feature
@@ -20,3 +20,5 @@ def head_word_of_pp(parsed_sentence):
                 feature = f"PP-{token_info['form']}"  #otherwise it only includes the preposition
             features.append(feature)  
     return features 
+
+
