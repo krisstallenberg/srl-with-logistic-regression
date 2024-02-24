a88e6e5825f0befe33cfc147c1079a8b4150c691
Farnaz-BNF <160543501+Farnaz-BNF@users.noreply.github.com>
2024-02-24 21:41:36 +0330

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -3,33 +3,6 @@
 
 from sklearn.metrics import classification_report, ConfusionMatrixDisplay, confusion_matrix
 
-# extract gold list from data
-def extract_golds_from_data(data):
-    """
-    extract gold label from input data.
-
-    Parameters
-    ----------
-    data 
-        Train or test or dev dataset (after extracting fetures)
-    
-    Returns
-    -------
-        List of Gold labels
-    """
-    labels_train = []
-    # for each sentence
-    for sentence in data:
-        # for each word in sentence
-        for word in sentence:
-            # for each feature of word
-            for item in word.items():
-                # if feature is argument 
-                if item[0] == 'argument':
-                    # append feature value to list of labels
-                    labels_train.append(item[1])
-    return labels_train
-
 def provide_confusion_matrix(GoldLabel, PredictLabel, label_set):
     """
     use `sklearn.metric confusion_matrix` to create confusion matrix of model predict.
@@ -79,7 +52,7 @@ def calculate_precision_recall_f1score(GoldLabel, PredictLabel, label_set): # fu
     return report # return report (maybe useful later)
 
 # def evaluation_model(GoldLabel, PredictLabel): # get gold and predict
-def evaluation_model(data, PredictLabel): # get gold and predict
+def evaluation_model(GoldLabel, PredictLabel): # get gold and predict
     """
     Evaluation models by call `calculate_precision_recall_f1score` and `provide_confusion_matrix` functions.
 
@@ -94,8 +67,6 @@ def evaluation_model(data, PredictLabel): # get gold and predict
     -------
         Classification report and Confusion matrix
     """
-
-    GoldLabel = extract_golds_from_data(data) # First extract Gold labels from data 
 
     label_set = sorted(set(GoldLabel)) # find uniqe lables in gold
     print(label_set)
